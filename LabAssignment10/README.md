fae99d6930620fa0e611688e7e42f9af7eb15ce0
SaiTejasweeReddy <snigdhareddy.95@gmail.com>
2018-02-13 15:34:56 -0600

    ASE

---
+++ LabAssignment10/README.md
@@ -0,0 +1,4 @@
+# ASE_labAssignment10
+
+Wiki page link: 
+https://github.com/SaiTejasweeReddy/ASE_labAssignment10/wiki/lab10wiki
---
+++ LabAssignment3/source/texttospeech.js
@@ -0,0 +1,7 @@
+function convertTextToSpeech(){
+    var text=document.getElementById("text").value;
+    var textToSpeechUrl='https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?username=e56cbad1-cf4e-47e4-a882-9b1732ffcaa5&password=sn76K1w3Smnl&text='+text;
+
+    var audio="<video controls='' autoplay='' name='media'><source src='"+textToSpeechUrl+"' type='audio/ogg'></video>";
+    document.getElementById("playAudio").innerHTML=audio;
+}
